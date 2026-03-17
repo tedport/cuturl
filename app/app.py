35a29d73d4daed369341a1a2ba04daa8afeffce6
Władysław Tarasow <97189300+tedport@users.noreply.github.com>
2026-03-17 18:17:11 +0100

    exceptions registrations

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
-import fastapi
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from app.routers import redirect, links
+from app.core.exceptions import register_exception_handlers
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+register_exception_handlers(app)
 
 @app.get("/static/favicon.ico", include_in_schema=False)
 async def icon():
